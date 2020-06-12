a4ed0ef08ebbc06112a48b83420fce64002d81d5
ktrain0 <59453532+ktrain0@users.noreply.github.com>
2020-06-11 22:53:33 -0700

    working version

--- main.py
+++ main.py
@@ -1,58 +1,64 @@
 import cv2
 import numpy as np
 
-widthImg = 480
-heightImg = 640
 
+###################################
+widthImg=540
+heightImg =640
+#####################################
 
-frameWidth = 640
-frameHeight = 480
-cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
+cap = cv2.VideoCapture(1)
 cap.set(10,150)
 
 
 def preProcessing(img):
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imgBlur = cv2.GaussianBlur(imgGray,(5,5), 1)
-    imgCanny = cv2.Canny(imgBlur, 200, 200)
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
+    imgCanny = cv2.Canny(imgBlur,200,200)
     kernel = np.ones((5,5))
-    imgDial = cv2.dilate(imgCanny, kernel, iterations = 2)
-    imgResult = cv2.erode(imgDial, kernel, iterations = 1)
-
-    return imgResult
-
-
+    imgDial = cv2.dilate(imgCanny,kernel,iterations=2)
+    imgThres = cv2.erode(imgDial,kernel,iterations=1)
+    return imgThres
 
 def getContours(img):
     biggest = np.array([])
     maxArea = 0
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area>5000:
-            peri = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
-            if area > maxArea and len(approx) == 4:
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            if area >maxArea and len(approx) == 4:
                 biggest = approx
                 maxArea = area
-
-    cv2.drawContours(imgContours, biggest, -1, (255,0,0), 20)
+    cv2.drawContours(imgContour, biggest, -1, (255, 0, 0), 20)
     return biggest
 
-
-def reorder(myPoints):
+def reorder (myPoints):
     myPoints = myPoints.reshape((4,2))
-    myPointsNew = np.zeros((4,1,2), np.int32)
+    myPointsNew = np.zeros((4,1,2),np.int32)
     add = myPoints.sum(1)
+    #print("add", add)
     myPointsNew[0] = myPoints[np.argmin(add)]
     myPointsNew[3] = myPoints[np.argmax(add)]
-    diff = np.diff(myPoints, axis = 1)
-    myPoints[1] = myPoints[np.argmin(diff)]
-    myPoints[2] = myPoints[np.argmax(diff)]
+    diff = np.diff(myPoints,axis=1)
+    myPointsNew[1]= myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    #print("NewPoints",myPointsNew)
     return myPointsNew
 
+def getWarp(img,biggest):
+    biggest = reorder(biggest)
+    pts1 = np.float32(biggest)
+    pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+    imgCropped = imgOutput[20:imgOutput.shape[0]-20,20:imgOutput.shape[1]-20]
+    imgCropped = cv2.resize(imgCropped,(widthImg,heightImg))
+
+    return imgCropped
 
 
 def stackImages(scale,imgArray):
@@ -86,44 +92,25 @@ def stackImages(scale,imgArray):
         ver = hor
     return ver
 
-
-
-def getWarp(img,biggest):
-    biggest = reorder(biggest)
-    pts1 = np.float32(biggest)
-    pts2 = np.float32([[0,0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
-    matrix = cv2.getPerspectiveTransform(pts1, pts2)
-    imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
-
-    imgCrop = imgOutput[20:imgOutput.shape[0] - 20, 20, imgOutput.shape[1] - 20]
-    imgCrop = cv2.resive(imgCrop, (widthImg, heightImg))
-
-    return imgCrop
-
-
-
 while True:
-    success, img = cap.read()
-    img = cv2.resize(img,(widthImg, heightImg))
-    imgContours = img.copy()
+    # success, img = cap.read()
+    img = cv2.imread(r'.\1.jpg')
+    img = cv2.resize(img,(widthImg,heightImg))
+    imgContour = img.copy()
 
-
-    imgThresh = preProcessing(img)
-    biggest = getContours(imgThresh)
-
-    if biggest.size != 0:
-        imgWarped = getWarp(img, biggest)
-
-        imgAry = ([img, imgThresh],
-                [imgContours,imgWarped])
-        cv2.imshow("Result", imgWarped)
+    imgThres = preProcessing(img)
+    biggest = getContours(imgThres)
+    if biggest.size !=0:
+        imgWarped=getWarp(img,biggest)
+        imageArray = ([img, imgContour],
+                      [imgThres, imgWarped])
+        cv2.imshow("ImageWarped", imgWarped)
     else:
-        imgAry = ([img, imgThresh],
-                [img,img])
+        imageArray = ([img, imgContour],
+                      [imgThres, img])
 
+    stackedImages = stackImages(0.6,imageArray)
+    cv2.imshow("WorkFlow", stackedImages)
 
-    stacked = stackImages(0.6, imgAry)
-    cv2.imshow("WorkFlow", stacked)
-    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
